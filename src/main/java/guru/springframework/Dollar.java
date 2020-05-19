c8d4fdfd6833a369fae09027ad18040e6d161df1
Orkhan Ismayilov <orxanismayilov92@gmail.com>
2020-05-19 17:01:38 +0400

    First commit

---
+++ src/main/java/guru/springframework/Dollar.java
@@ -0,0 +1,17 @@
+package guru.springframework;
+
+import lombok.Data;
+
+@Data
+public class Dollar {
+
+    private int amount;
+
+    public Dollar(int amount) {
+        this.amount = amount;
+    }
+
+    public Dollar times(int multiplier) {
+        return new Dollar(amount * multiplier);
+    }
+}
